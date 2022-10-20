cddc19b611e130eb5ea80b4a7adb9bf0d3bc4f82
andmatcha <jj.mcr185@gmail.com>
2022-10-20 16:40:36 +0900

    feat: :sparkles: 入力をそのまま出力できた

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
-use std::io::{stdin, stdout, Write, BufWriter};
+use std::io::{stdin, stdout, BufWriter, Write};
 
 fn main() {
+    let (mut num1, mut num2, mut num3, mut num4) =
+        (String::new(), String::new(), String::new(), String::new());
+
     print!("Enter first number[0-9]: ");
     stdout().flush().unwrap();
-    let mut num1 = String::new();
     stdin().read_line(&mut num1).expect("error");
 
     print!("Enter second number[0-9]: ");
     stdout().flush().unwrap();
-    let mut num2 = String::new();
     stdin().read_line(&mut num2).expect("error");
 
     print!("Enter third number[0-9]: ");
     stdout().flush().unwrap();
-    let mut num3 = String::new();
     stdin().read_line(&mut num3).expect("error");
 
     print!("Enter fourth number[0-9]: ");
     stdout().flush().unwrap();
-    let mut num4 = String::new();
     stdin().read_line(&mut num4).expect("error");
+
+    let numbers = [num1, num2, num3, num4];
+    let mut new_numbers: Vec<i32> = vec![];
+
+    for number in numbers {
+        let new_number: i32 = number.trim().parse().expect("error");
+        new_numbers.push(new_number);
+    }
 
     let out = stdout();
     let mut out = BufWriter::new(out.lock());
 
-    write!(out, "You entered {} {} {} {}", num1, num2, num3, num4).expect("error");
+    writeln!(out, "You entered {:?}", new_numbers).expect("error");
 }
