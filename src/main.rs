caac2b24f5bc518c67bab3aa57ccab426e9d2d51
andmatcha <jj.mcr185@gmail.com>
2022-10-20 16:56:02 +0900

    perf: :zap: 無駄なロジックを消去

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{stdin, stdout, BufWriter, Write};
+use std::io::{stdin, stdout, Write};
 
 fn main() {
     let (mut num1, mut num2, mut num3, mut num4) =
@@ -20,16 +20,5 @@ fn main() {
     stdout().flush().unwrap();
     stdin().read_line(&mut num4).expect("error");
 
-    let numbers = [num1, num2, num3, num4];
-    let mut new_numbers: Vec<i32> = vec![];
-
-    for number in numbers {
-        let new_number: i32 = number.trim().parse().expect("error");
-        new_numbers.push(new_number);
-    }
-
-    let out = stdout();
-    let mut out = BufWriter::new(out.lock());
-
-    writeln!(out, "You entered {:?}", new_numbers).expect("error");
+    println!("You entered {}, {}, {}, {}", num1.trim(), num2.trim(), num3.trim(), num4.trim());
 }
